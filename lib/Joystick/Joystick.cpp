a510e3b9678a7e1ab4f3a6ed27cddfe84a0f9e81
Andrewekwie <151432419+Andrewekwie@users.noreply.github.com>
2025-05-02 16:08:53 +0300

    Update Joystick.cpp

--- lib/Joystick/Joystick.cpp
+++ lib/Joystick/Joystick.cpp
@@ -35,24 +35,23 @@ void Joystick::update()
   int scaledX = 0;
   int scaledY = 0;
 
-  // Ensure minor x-input deviations don't cause movement
+
   if(abs(xInitValue-rawX) >= 50) {
 
     if(rawX > xInitValue)
-      scaledX = map(rawX, xInitValue, 4095, 0, 100); //RIGHT
+      scaledX = map(rawX, xInitValue, 4095, 0, 100); 
     else
-      scaledX = map(rawX, 0, xInitValue, -100, 0); //LEFT
+      scaledX = map(rawX, 0, xInitValue, -100, 0); 
   }
 
-  // Ensure minor y-input deviations don't cause movement
   if(abs(yInitValue-rawY) >= 35) {
     if(rawY > yInitValue)
-      scaledY = map(rawY, yInitValue, 4095, 0, 100); //UP
+      scaledY = map(rawY, yInitValue, 4095, 0, 100); 
     else
-      scaledY = map(rawY, 0, yInitValue, -100, 0); //DOWN
+      scaledY = map(rawY, 0, yInitValue, -100, 0); 
   }
   
-  // Adjust values based on the joystick orientation
+
   switch(rotation){
     case ROTATION_0:
       xValue = scaledX;
@@ -72,8 +71,7 @@ void Joystick::update()
       break;
   }
 
-  // Track the button's previous value to determine if it has changed, otherwise
-  // buttonPressed() will be true on every loop while the button is pressed.
+
   buttonPreviousValue = buttonValue;
   buttonValue = digitalRead(swPin);
 
@@ -95,9 +93,6 @@ bool Joystick::buttonPressed()
   return buttonValue == 0 && buttonValue != buttonPreviousValue;
 }
 
-// Sometimes we want a distinct direction, not a combination
-// This could use some adjusting, but the values would vary from
-// one joystick to the next.
 Direction Joystick::direction()
 {
   if(xValue < -40)
